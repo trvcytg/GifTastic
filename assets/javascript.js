9562776ba1074a38750d8fb0973603468aa18d09
Todd Tracy <trvcytg@gmail.com>
2019-09-04 14:37:15 -0500

    Created initial framework for website, wrote JS to create topics array and push Buttons to page based on array, created style.css

---
+++ assets/javascript.js
@@ -0,0 +1,13 @@
+var topics = [`nature`, `cats`, `dogs`, `funny`];
+let buttonPrint = $(`#buttonPrint`);
+
+topics.forEach(function(element) {
+  var newButton = $(`<button>`);
+  newButton.attr(`type`, `button`);
+  newButton.attr(`class`, `btn btn-success`);
+  newButton.attr(`id`, element);
+  //   Found code for Capitalize first letter of String: https://joshtronic.com/2016/02/14/how-to-capitalize-the-first-letter-in-a-string-in-javascript/
+  element = element.charAt(0).toUpperCase() + element.substring(1);
+  newButton.append(element);
+  buttonPrint.append(newButton);
+});
